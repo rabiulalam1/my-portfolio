c7fda4e32fea480c82079a9adbecde95f9790384
rabiulalam1 <rabiul.alam@outlook.com>
2020-08-24 11:15:10 -0400

    Tets

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// Header Scrolling Effect
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Sliding Menu Indicator
+
+let marker = document.querySelector(".marker");
+let menuItem = document.querySelectorAll("nav a");
+
+function indicator(e) {
+  marker.classList.add("marker-active");
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+
+menuItem.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    indicator(e.target);
+  });
+});
